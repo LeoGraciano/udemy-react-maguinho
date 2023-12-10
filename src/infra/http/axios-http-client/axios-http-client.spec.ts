391c29549b461cc30930a17b4cb33741206e753f
Léo Graciano <leonardoferreiragraciano@gmail.com>
2023-12-10 10:00:59 -0300

    refactor: move post request creation to a factory method

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,3 +1,4 @@
+import { HttpPostParams } from "@/data/protocols/http";
 import { faker } from "@faker-js/faker";
 import axios from "axios";
 import { AxiosHttpClient } from "./axios-https-client";
@@ -10,11 +11,41 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
 };
 
+interface RandomObject {
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  phone: string;
+}
+
+const mockRandomObjectPerson = (): RandomObject => {
+  const newRandomObject = {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    address: {
+      street: faker.location.streetAddress(),
+      city: faker.location.city(),
+      country: faker.location.country(),
+    },
+    phone: faker.phone.number(),
+  };
+  return newRandomObject;
+};
+
+const mockPostRequst = (): HttpPostParams<unknown> => ({
+  url: faker.internet.url(),
+  body: mockRandomObjectPerson,
+});
+
 describe("AxiosHttpClient", () => {
   test("Should call axios with correct URL and verb POST", async () => {
-    const url = faker.internet.url();
+    const request = mockPostRequst();
     const sut = makeSut();
-    await sut.post({ url });
-    expect(mockedAxios.post).toHaveBeenCalledWith(url);
+    await sut.post(request);
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url);
   });
 });
