a3676f0533799b5740f8263bd813a40d5c0ef926
Léo Graciano <leonardoferreiragraciano@gmail.com>
2023-12-10 10:04:47 -0300

    test: ensure AxiosHttpClient calls axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -36,16 +36,16 @@ const mockRandomObjectPerson = (): RandomObject => {
   return newRandomObject;
 };
 
-const mockPostRequst = (): HttpPostParams<unknown> => ({
+const mockPostRequest = (): HttpPostParams<unknown> => ({
   url: faker.internet.url(),
   body: mockRandomObjectPerson,
 });
 
 describe("AxiosHttpClient", () => {
-  test("Should call axios with correct URL and verb POST", async () => {
-    const request = mockPostRequst();
+  test("Should call axios with correct values", async () => {
+    const request = mockPostRequest();
     const sut = makeSut();
     await sut.post(request);
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
   });
 });
